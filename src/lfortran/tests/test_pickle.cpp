5abc5a57be2a9690989789583e43af3846d8da1c
Ondřej Čertík <ondrej@certik.us>
2019-08-23 19:04:37 -0600

    Add more tests

--- src/lfortran/tests/test_pickle.cpp
+++ src/lfortran/tests/test_pickle.cpp
@@ -113,6 +113,9 @@ TEST_CASE("Subroutines") {
     end subroutine)")   == "(sub [] [(= x y) (= x (* 2 y))])");
 
     CHECK(P(R"(subroutine g
+    end subroutine)")   == "(sub [] [])");
+
+    CHECK(P(R"(subroutine g
     x = y
 
     x = 2*y
@@ -161,6 +164,9 @@ TEST_CASE("Functions") {
     x = 2*y
     end function)")   == "(fn [] [(= x y) (= x (* 2 y))])");
 
+    CHECK(P(R"(function g
+    end function)")   == "(fn [] [])");
+
 
     CHECK(P(R"(function g
     x = y; ;
@@ -185,6 +191,9 @@ TEST_CASE("Programs") {
     x = y
     x = 2*y
     end program)")   == "(prog g [] [(= x y) (= x (* 2 y))])");
+
+    CHECK(P(R"(program g
+    end program)")   == "(prog g [] [])");
 
 
     CHECK(P(R"(program g
@@ -315,6 +324,31 @@ TEST_CASE("if") {
         b = 4
     end if
     end subroutine)")   == "(sub [] [(if x [(= a 5)] [(= b 4)])])");
+
+    CHECK(P(R"(subroutine g
+    if (x) then
+    else
+        b = 4
+    end if
+    end subroutine)")   == "(sub [] [(if x [] [(= b 4)])])");
+
+    CHECK(P(R"(subroutine g
+    if (x) then
+        a = 5
+    else
+    end if
+    end subroutine)")   == "(sub [] [(if x [(= a 5)] [])])");
+
+    CHECK(P(R"(subroutine g
+    if (x) then
+    else
+    end if
+    end subroutine)")   == "(sub [] [(if x [] [])])");
+
+    CHECK(P(R"(subroutine g
+    if (x) then
+    end if
+    end subroutine)")   == "(sub [] [(if x [] [])])");
 
     CHECK(P(R"(subroutine g
     if (x) then
@@ -468,6 +502,10 @@ TEST_CASE("do loop") {
  R"(do i = 1, 5
         a = a + i
     end do)") == "(do i 1 5 () [(= a (+ a i))])");
+
+    CHECK(P(
+ R"(do i = 1, 5
+    end do)") == "(do i 1 5 () [])");
 
     CHECK(P(
  R"(do i = 1, 5, 2
