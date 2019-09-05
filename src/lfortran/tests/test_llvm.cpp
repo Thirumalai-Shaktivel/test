0b8a83dd94d4cb385f428f6d07733e359ffb8e21
Ondřej Čertík <ondrej@certik.us>
2019-09-04 23:55:59 -0600

    Simplify headers

--- src/lfortran/tests/test_llvm.cpp
+++ src/lfortran/tests/test_llvm.cpp
@@ -1,46 +1,5 @@
-#include "llvm/ADT/STLExtras.h"
-#include "llvm/Analysis/Passes.h"
-#include "llvm/ExecutionEngine/ExecutionEngine.h"
-#include "llvm/ExecutionEngine/GenericValue.h"
-#include "llvm/ExecutionEngine/MCJIT.h"
-#include "llvm/IR/Argument.h"
-#include "llvm/IR/Attributes.h"
-#include "llvm/IR/BasicBlock.h"
-#include "llvm/IR/Constants.h"
-#include "llvm/IR/DerivedTypes.h"
-#include "llvm/IR/Function.h"
-#include "llvm/IR/IRBuilder.h"
-#include "llvm/IR/Instructions.h"
-#include "llvm/IR/Intrinsics.h"
-#include "llvm/IR/LegacyPassManager.h"
 #include "llvm/IR/LLVMContext.h"
-#include "llvm/IR/Module.h"
-#include "llvm/IR/Type.h"
-#include "llvm/Support/Casting.h"
-#include "llvm/Support/ManagedStatic.h"
-#include "llvm/Support/TargetSelect.h"
-#include "llvm/Support/raw_ostream.h"
-#include "llvm/ADT/APFloat.h"
-#include "llvm/ADT/STLExtras.h"
-#include "llvm/IR/Verifier.h"
-#include "llvm/Support/TargetSelect.h"
-#include "llvm/Target/TargetMachine.h"
-#include "llvm/Transforms/Scalar.h"
-#include <llvm/Transforms/Scalar/GVN.h>
-#include "llvm/Transforms/Vectorize.h"
-#include "llvm/ExecutionEngine/ObjectCache.h"
-#include "llvm/Support/FileSystem.h"
-#include "llvm/Support/Path.h"
 
-
-#include <algorithm>
-#include <cctype>
-#include <cstdio>
-#include <cstdlib>
-#include <map>
-#include <memory>
-#include <string>
-#include <vector>
 #include <iostream>
 
 int main() {
