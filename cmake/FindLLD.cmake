b8d175d5391f9599fe683ebd9aeeb7a99c4b7ed2
Ondřej Čertík <ondrej@certik.us>
2020-07-31 18:30:33 -0600

    Make LLD depend on LLVM

--- cmake/FindLLD.cmake
+++ cmake/FindLLD.cmake
@@ -1,3 +1,5 @@
+find_package(LLVM REQUIRED)
+
 find_path(LLD_INCLUDE_DIR lld/Common/Driver.h HINTS ${LLVM_INCLUDE_DIR})
 find_library(LLD_LIBRARY_ELF lldELF HINTS ${LLVM_LIBRARY_DIR})
 find_library(LLD_LIBRARY_COMMON lldCommon HINTS ${LLVM_LIBRARY_DIR})
@@ -13,3 +15,4 @@ set_property(TARGET p::lld PROPERTY INTERFACE_COMPILE_OPTIONS
     $<$<COMPILE_LANGUAGE:CXX>:${LFORTRAN_CXX_NO_RTTI_FLAG}>)
 set_property(TARGET p::lld PROPERTY INTERFACE_LINK_LIBRARIES
     ${LLD_LIBRARY_ELF} ${LLD_LIBRARY_COMMON})
+target_link_libraries(p::lld INTERFACE p::llvm)
