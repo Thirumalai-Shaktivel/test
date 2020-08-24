0858f702523f0621fe42a17a67cd53d98f3df1b9
Ondřej Čertík <ondrej@certik.us>
2020-08-24 15:43:08 -0600

    XX find static library first

--- cmake/FindBFD.cmake
+++ cmake/FindBFD.cmake
@@ -1,5 +1,5 @@
 find_path(BFD_INCLUDE_DIR bfd.h)
-find_library(BFD_LIBRARY bfd)
+find_library(BFD_LIBRARY libbfd.a)
 #find_library(IBERTY_LIBRARY iberty)
 #find_library(Z_LIBRARY z)
 #find_library(DL_LIBRARY dl)
