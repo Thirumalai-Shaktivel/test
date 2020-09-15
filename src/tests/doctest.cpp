bd00af0ab8b9a7b86cf2797f9da6d7fae1437ea8
Ondřej Čertík <ondrej@certik.us>
2020-09-15 16:54:37 -0600

    Implement stacktraces in doctest

--- src/tests/doctest.cpp
+++ src/tests/doctest.cpp
@@ -1,2 +1,14 @@
-#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#define DOCTEST_CONFIG_NO_POSIX_SIGNALS
+#define DOCTEST_CONFIG_IMPLEMENT
 #include "doctest.h"
+
+#include <lfortran/config.h>
+#include <lfortran/stacktrace.h>
+
+int main(int argc, char** argv)
+{
+#if defined(HAVE_LFORTRAN_STACKTRACE)
+    LFortran::print_stack_on_segfault();
+#endif
+    return doctest::Context(argc, argv).run();
+}
