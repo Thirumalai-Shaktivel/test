5cada29f171364bb6f32183e7ae586fd836e88e2
Ondřej Čertík <ondrej@certik.us>
2020-09-16 11:53:22 -0600

    Also works

--- src/tests/doctest.cpp
+++ src/tests/doctest.cpp
@@ -12,22 +12,7 @@
 int main(int argc, char** argv)
 {
 #if defined(HAVE_LFORTRAN_STACKTRACE)
-    //LFortran::print_stack_on_segfault();
+    LFortran::print_stack_on_segfault();
 #endif
-    try {
-        return doctest::Context(argc, argv).run();
-    } catch(const LFortran::LFortranException &e) {
-        std::cerr << e.stacktrace();
-        std::cerr << e.name() + ": " << e.msg() << std::endl;
-        return 1;
-    } catch(const std::runtime_error &e) {
-        std::cerr << "runtime_error: " << e.what() << std::endl;
-        return 1;
-    } catch(const std::exception &e) {
-        std::cerr << "std::exception: " << e.what() << std::endl;
-        return 1;
-    } catch(...) {
-        std::cerr << "Unknown Exception" << std::endl;
-        return 1;
-    }
+    return doctest::Context(argc, argv).run();
 }
