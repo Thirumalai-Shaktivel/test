2c00b30c280171ec8df0237dcb1a488545590a42
Ondřej Čertík <ondrej@certik.us>
2020-10-26 16:04:33 -0600

    No need to pass nopie

--- comp.sh
+++ comp.sh
@@ -4,4 +4,4 @@ set -ex
 
 lfortran --show-llvm integration_tests/reserved_02.f90 > reserved_02.ll
 llc -filetype=obj reserved_02.ll
-clang -o a.out reserved_02.o -L/Users/ondrej/repos/lfortran/lfortran/src/bin/../runtime -Wl,-no_pie -Wl,-rpath,/Users/ondrej/repos/lfortran/lfortran/src/bin/../runtime -llfortran_runtime_static -lm
+clang -o a.out reserved_02.o -L/Users/ondrej/repos/lfortran/lfortran/src/bin/../runtime -Wl,-rpath,/Users/ondrej/repos/lfortran/lfortran/src/bin/../runtime -llfortran_runtime_static -lm
