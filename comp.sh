4244321d03e3794a79e5112ba8afd3b302a2ba13
Ondřej Čertík <ondrej@certik.us>
2020-11-02 23:58:36 -0700

    Use gcc and pie

--- comp.sh
+++ comp.sh
@@ -4,4 +4,4 @@ set -ex
 
 lfortran --show-llvm integration_tests/reserved_02.f90 > reserved_02.ll
 llc -mtriple=x86_64-apple-darwin19.5.0 -filetype=obj reserved_02.ll
-clang -o a.out reserved_02.o -L/Users/ondrej/repos/lfortran/lfortran/src/bin/../runtime -Wl,-rpath,/Users/ondrej/repos/lfortran/lfortran/src/bin/../runtime -llfortran_runtime_static -lm
+gcc -o a.out reserved_02.o -L/Users/ondrej/repos/lfortran/lfortran/src/bin/../runtime -Wl,-rpath,/Users/ondrej/repos/lfortran/lfortran/src/bin/../runtime -llfortran_runtime_static -lm
