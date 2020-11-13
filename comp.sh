10901c8ed14ff827f56524378130482facbbb113
Ondřej Čertík <ondrej@certik.us>
2020-11-12 17:09:33 -0700

    Create an object file

--- comp.sh
+++ comp.sh
@@ -7,5 +7,6 @@ llc -mtriple=x86_64-apple-darwin19.5.0 -filetype=obj reserved_02.ll
 gcc -o a.out reserved_02.o -L/Users/ondrej/repos/lfortran/lfortran/src/bin/../runtime -Wl,-rpath,/Users/ondrej/repos/lfortran/lfortran/src/bin/../runtime -llfortran_runtime_static -lm
 ./a.out
 
-lfortran integration_tests/reserved_02.f90 -o a2.out
+lfortran -c integration_tests/reserved_02.f90 -o reserved_02b.o
+gcc -o a2.out reserved_02b.o -L/Users/ondrej/repos/lfortran/lfortran/src/bin/../runtime -Wl,-rpath,/Users/ondrej/repos/lfortran/lfortran/src/bin/../runtime -llfortran_runtime_static -lm
 ./a2.out
