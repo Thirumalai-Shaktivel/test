059409123ad79e3e32d1392390dd746a1c3402d8
Ondřej Čertík <ondrej@certik.us>
2020-11-12 17:07:13 -0700

    Run and try direct

--- comp.sh
+++ comp.sh
@@ -5,3 +5,7 @@ set -ex
 lfortran --show-llvm integration_tests/reserved_02.f90 > reserved_02.ll
 llc -mtriple=x86_64-apple-darwin19.5.0 -filetype=obj reserved_02.ll
 gcc -o a.out reserved_02.o -L/Users/ondrej/repos/lfortran/lfortran/src/bin/../runtime -Wl,-rpath,/Users/ondrej/repos/lfortran/lfortran/src/bin/../runtime -llfortran_runtime_static -lm
+./a.out
+
+lfortran integration_tests/reserved_02.f90 -o a2.out
+./a2.out
