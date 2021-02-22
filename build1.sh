19b1a5c30cbd32f20a6e8588cb237f4551664186
czgdp1807 <gdp.1807@gmail.com>
2021-02-22 14:07:06 +0530

    Custom instal dir

--- build1.sh
+++ build1.sh
@@ -3,11 +3,20 @@
 set -e
 set -x
 
+install_path="inst"
+
+if [ ! -z "$1" ]
+then
+    install_path="$1"
+fi
+
+echo $install_path
+
 cmake \
     -DCMAKE_BUILD_TYPE=Debug \
     -DWITH_LLVM=yes \
     -DWITH_STACKTRACE=yes \
     -DCMAKE_PREFIX_PATH="$CMAKE_PREFIX_PATH;$CONDA_PREFIX" \
-    -DCMAKE_INSTALL_PREFIX=`pwd`/inst \
+    -DCMAKE_INSTALL_PREFIX=$install_path \
     .
 cmake --build . --target install
