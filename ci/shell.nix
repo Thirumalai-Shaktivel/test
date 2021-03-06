90cf9e1d3a44bc100f92c49f325b711f7d683991
Rohit Goswami <rgoswami@ieee.org>
2021-03-06 16:19:31 +0000

    gitig: Update

--- ci/shell.nix
+++ ci/shell.nix
@@ -1,7 +1,7 @@
+{ clangSupport ? true }:
 let
   sources = import ./nix/sources.nix;
   pkgs = import sources.nixpkgs { };
-# python=3.7 re2c m2-bison xonsh llvmdev=11.1.0 jupyter xeus xtl nlohmann_json cppzmq jupyter_kernel_test pytest
   mach-nix = import (builtins.fetchGit {
     url = "https://github.com/DavHau/mach-nix/";
     ref = "refs/tags/3.1.1";
@@ -15,21 +15,24 @@ let
       toml
     '';
   };
-  mkShellNewEnv = pkgs.mkShell.override { stdenv = pkgs.gcc10Stdenv; };
+  llvmPkgs = pkgs.buildPackages.llvmPackages_11;
+  myStdenv = if clangSupport then llvmPkgs.stdenv else pkgs.gcc10Stdenv;
+  mkShellNewEnv = pkgs.mkShell.override { stdenv = myStdenv; };
 in mkShellNewEnv {
   nativeBuildInputs = [ pkgs.cmake ];
   buildInputs = with pkgs; [
     customPython
-    gtest
     bashInteractive
     which
-    gcc10Stdenv
     gfortran
     valgrind
     gdb
-
+    fmt
     llvm_11
+    lld_11
+    llvmPkgs.bintools
     bison_3_5
+    zlib
     libbfd
     re2c
     git
