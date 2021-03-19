5e6b871844d64dfddd0de72da1680e2d5f3aed29
Ondřej Čertík <ondrej@certik.us>
2021-03-19 10:12:40 -0600

    Read from a file

--- grammar/driver.c
+++ grammar/driver.c
@@ -1,6 +1,7 @@
 #include "pl0.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 extern unsigned char *cur;
 
@@ -10,10 +11,89 @@ void error(const char msg[])
     exit(1);
 }
 
-int main()
+
+/*
+ * This function was taken from: https://stackoverflow.com/a/22515917/479532
+ *
+ * 'slurp' reads the file identified by 'path' into a character buffer
+ * pointed at by 'buf', optionally adding a terminating NUL if
+ * 'add_nul' is true. On success, the size of the file is returned; on
+ * failure, -1 is returned and ERRNO is set by the underlying system
+ * or library call that failed.
+ *
+ * WARNING: 'slurp' malloc()s memory to '*buf' which must be freed by
+ * the caller.
+ *
+ */
+long slurp(char const* path, char **buf, bool add_nul)
 {
-    char *s = "x := 1+(1+x*3-4)*4/n .";
-    cur = (unsigned char*)s;
+    FILE  *fp;
+    size_t fsz;
+    long   off_end;
+    int    rc;
+
+    /* Open the file */
+    fp = fopen(path, "rb");
+    if( NULL == fp ) {
+        return -1L;
+    }
+
+    /* Seek to the end of the file */
+    rc = fseek(fp, 0L, SEEK_END);
+    if( 0 != rc ) {
+        return -1L;
+    }
+
+    /* Byte offset to the end of the file (size) */
+    if( 0 > (off_end = ftell(fp)) ) {
+        return -1L;
+    }
+    fsz = (size_t)off_end;
+
+    /* Allocate a buffer to hold the whole file */
+    *buf = malloc( fsz+(int)add_nul );
+    if( NULL == *buf ) {
+        return -1L;
+    }
+
+    /* Rewind file pointer to start of file */
+    rewind(fp);
+
+    /* Slurp file into buffer */
+    if( fsz != fread(*buf, 1, fsz, fp) ) {
+        free(*buf);
+        return -1L;
+    }
+
+    /* Close the file */
+    if( EOF == fclose(fp) ) {
+        free(*buf);
+        return -1L;
+    }
+
+    if( add_nul ) {
+        /* Make sure the buffer is NUL-terminated, just in case */
+        buf[fsz] = (char*)'\0';
+    }
+
+    /* Return the file size */
+    return (long)fsz;
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc < 2) {
+        printf("Usage: ./pl0 <filename>\n");
+        exit(1);
+    }
+    long file_size;
+    char *buf;
+    file_size = slurp(argv[1], &buf, false);
+    if (file_size < 0) {
+        printf("File '%s' read failed.\n", argv[1]);
+        exit(1);
+    }
+    cur = (unsigned char*)buf;
     nextsym();
     program();
     printf("OK\n");
