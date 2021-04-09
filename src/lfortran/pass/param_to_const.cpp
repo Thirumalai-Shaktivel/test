7f7bd0741e836d6e83098bd2e9ebbaa5fb86eb08
czgdp1807 <gdp.1807@gmail.com>
2021-04-09 12:11:38 +0530

    removed expr? value from expr

--- src/lfortran/pass/param_to_const.cpp
+++ src/lfortran/pass/param_to_const.cpp
@@ -32,32 +32,10 @@ class VarVisitor : public ASR::BaseWalkVisitor<VarVisitor>
 {
 private:
     Allocator &al;
-    Vec<ASR::stmt_t*> init_var_result;
+
 public:
 
     VarVisitor(Allocator &al) : al{al} {
-        init_var_result.n = 0;
-    }
-
-    void transform_stmts(ASR::stmt_t **&m_body, size_t &n_body) {
-        Vec<ASR::stmt_t*> body;
-        body.reserve(al, n_body);
-        for (size_t i=0; i<n_body; i++) {
-            // Not necessary after we check it after each visit_stmt in every
-            // visitor method:
-            init_var_result.n = 0;
-            visit_stmt(*m_body[i]);
-            if (init_var_result.size() > 0) {
-                for (size_t j=0; j<init_var_result.size(); j++) {
-                    body.push_back(al, init_var_result[j]);
-                }
-                init_var_result.n = 0;
-            } else {
-                body.push_back(al, m_body[i]);
-            }
-        }
-        m_body = body.p;
-        n_body = body.size();
     }
 
     // TODO: Only Program and While is processed, we need to process all calls
@@ -66,47 +44,12 @@ public:
     void visit_Program(const ASR::Program_t &x) {
         // FIXME: this is a hack, we need to pass in a non-const `x`,
         // which requires to generate a TransformVisitor.
-        ASR::Program_t &xx = const_cast<ASR::Program_t&>(x);
         for (auto &item : x.m_symtab->scope) {
             if (is_a<ASR::Variable_t>(*item.second)) {
                 ASR::Variable_t *v = down_cast<ASR::Variable_t>(item.second);
                 visit_Variable(*v);
             }
         }
-        transform_stmts(xx.m_body, xx.n_body);
-
-        // Transform nested functions and subroutines
-        for (auto &item : x.m_symtab->scope) {
-            if (is_a<ASR::Subroutine_t>(*item.second)) {
-                ASR::Subroutine_t *s = down_cast<ASR::Subroutine_t>(item.second);
-                visit_Subroutine(*s);
-            }
-            if (is_a<ASR::Function_t>(*item.second)) {
-                ASR::Function_t *s = down_cast<ASR::Function_t>(item.second);
-                visit_Function(*s);
-            }
-        }
-    }
-
-    void visit_Subroutine(const ASR::Subroutine_t &x) {
-        // FIXME: this is a hack, we need to pass in a non-const `x`,
-        // which requires to generate a TransformVisitor.
-        ASR::Subroutine_t &xx = const_cast<ASR::Subroutine_t&>(x);
-        transform_stmts(xx.m_body, xx.n_body);
-    }
-
-    void visit_Function(const ASR::Function_t &x) {
-        // FIXME: this is a hack, we need to pass in a non-const `x`,
-        // which requires to generate a TransformVisitor.
-        ASR::Function_t &xx = const_cast<ASR::Function_t&>(x);
-        transform_stmts(xx.m_body, xx.n_body);
-    }
-
-    void visit_WhileLoop(const ASR::WhileLoop_t &x) {
-        // FIXME: this is a hack, we need to pass in a non-const `x`,
-        // which requires to generate a TransformVisitor.
-        ASR::WhileLoop_t &xx = const_cast<ASR::WhileLoop_t&>(x);
-        transform_stmts(xx.m_body, xx.n_body);
     }
 
     void visit_Variable(const ASR::Variable_t& x) {
