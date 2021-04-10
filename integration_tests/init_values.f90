ae10c265ea071de53e4cb3c900d4b02e9a3c67a5
czgdp1807 <gdp.1807@gmail.com>
2021-04-10 12:17:54 +0530

    restoring init_values for time being

--- integration_tests/init_values.f90
+++ integration_tests/init_values.f90
@@ -1,9 +1,8 @@
 program init_values
 
-    integer, parameter :: i = 1.0, j = 2.0
+    integer :: i = 1.0
     real :: r = 4.0
     complex :: c = (3.0, 4.0)
-    integer, parameter :: a = i + j
-    print *, i, j, r, c, a
+    print *, i, r, c
 
 end program
