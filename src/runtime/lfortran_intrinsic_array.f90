75a03968dc5a4eecb39c90e647b7ed216edd10ae
Ondřej Čertík <ondrej@certik.us>
2021-05-30 13:12:27 -0600

    Make all functions interfaces

--- src/runtime/lfortran_intrinsic_array.f90
+++ src/runtime/lfortran_intrinsic_array.f90
@@ -5,71 +5,49 @@ interface
     integer function size(x) result(r)
     integer, intent(in) :: x(:)
     end function
-end interface
 
-interface
     integer function lbound(x, dim) result(r)
     integer, intent(in) :: x(:)
     integer, intent(in) :: dim
     end function
-end interface
 
-interface
     integer function ubound(x, dim) result(r)
     integer, intent(in) :: x(:)
     integer, intent(in) :: dim
     end function
+
+    logical function allocated(x) result(r)
+    real(8), intent(in) :: x(:)
+    end function
+
+    real(8) function real(x, knd) result(r)
+    integer, intent(in) :: x
+    integer, intent(in) :: knd
+    end function
+
+    real(8) function sum(x) result(r)
+    real(8), intent(in) :: x(:)
+    end function
+
+    real(8) function abs(x) result(r)
+    real(8), intent(in) :: x(:)
+    end function
+
+    integer function maxval(x) result(r)
+    real(8), intent(in) :: x(:)
+    end function
+
+    integer function minval(x) result(r)
+    real(8), intent(in) :: x(:)
+    end function
+
+    integer function max(a, b) result(r)
+    integer, intent(in) :: a, b
+    end function
+
+    integer function min(a, b) result(r)
+    integer, intent(in) :: a, b
+    end function
 end interface
-
-contains
-
-logical function allocated(x) result(r)
-real(8), intent(in) :: x(:)
-! TODO: implement allocated()
-r = .true.
-end function
-
-real(8) function real(x, knd) result(r)
-integer, intent(in) :: x
-integer, intent(in) :: knd
-! TODO: implement real()
-r = 0
-end function
-
-real(8) function sum(x) result(r)
-real(8), intent(in) :: x(:)
-! TODO: implement sum()
-r = 0
-end function
-
-real(8) function abs(x) result(r)
-real(8), intent(in) :: x(:)
-! TODO: implement abs()
-r = 0
-end function
-
-integer function maxval(x) result(r)
-real(8), intent(in) :: x(:)
-! TODO: implement maxval()
-r = 0
-end function
-
-integer function minval(x) result(r)
-real(8), intent(in) :: x(:)
-! TODO: implement minval()
-r = 0
-end function
-
-integer function max(a, b) result(r)
-integer, intent(in) :: a, b
-! TODO: implement max()
-r = 0
-end function
-
-integer function min(a, b) result(r)
-integer, intent(in) :: a, b
-! TODO: implement min()
-r = 0
-end function
 
 end module
