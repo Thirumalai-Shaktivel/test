9a4638a197a8a8e74a77ba8caabc97de91af04b9
Rohit Goswami <rog32@hi.is>
2021-07-20 07:19:08 +0000

    ci: Use ninja everywhere

--- build1.sh
+++ build1.sh
@@ -10,5 +10,5 @@ cmake \
     -DWITH_STACKTRACE=yes \
     -DCMAKE_PREFIX_PATH="$CMAKE_PREFIX_PATH_LFORTRAN;$CONDA_PREFIX" \
     -DCMAKE_INSTALL_PREFIX=`pwd`/inst \
-    .
-cmake --build . -j16 --target install
+    . -G "Ninja"
+cmake --build . --target install
