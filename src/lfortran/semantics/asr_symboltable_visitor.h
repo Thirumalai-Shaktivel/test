238f739e7d1c00d07cba74ef4d3e980c2da9d833
Rohit Goswami <rog32@hi.is>
2021-07-29 14:29:20 +0000

    tiny: Use a temporary simpler intrinsic module

--- src/lfortran/semantics/asr_symboltable_visitor.h
+++ src/lfortran/semantics/asr_symboltable_visitor.h
@@ -25,7 +25,7 @@ private:
       {"real", "lfortran_intrinsic_array"},
       {"sum", "lfortran_intrinsic_array"},
       {"abs", "lfortran_intrinsic_array"},
-      {"tiny", "lfortran_intrinsic_misc"}
+      {"tiny", "lfortran_intrinsic_tmp"}
 };
 
 public:
