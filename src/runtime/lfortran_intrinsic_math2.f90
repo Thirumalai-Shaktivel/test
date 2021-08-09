5672bc3bb456fdc24f45bf70fb32aba9ece90a61
Ondřej Čertík <ondrej@certik.us>
2021-08-09 16:00:40 -0600

    Comment out generic procedures for now

--- src/runtime/lfortran_intrinsic_math2.f90
+++ src/runtime/lfortran_intrinsic_math2.f90
@@ -3,20 +3,20 @@ module lfortran_intrinsic_math2
 use, intrinsic :: iso_fortran_env, only: sp => real32, dp => real64
 implicit none
 
-interface abs
-    module procedure sabs, dabs
-end interface
+!interface abs
+!    module procedure sabs, dabs
+!end interface
 
-interface sqrt
-    module procedure ssqrt, dsqrt
-end interface
+!interface sqrt
+!    module procedure ssqrt, dsqrt
+!end interface
 
 
 contains
 
 ! abs --------------------------------------------------------------------------
 
-elemental real(sp) function sabs(x) result(r)
+elemental real(sp) function abs(x) result(r)
 real(sp), intent(in) :: x
 if (x >= 0) then
     r = x
@@ -25,34 +25,34 @@ else
 end if
 end function
 
-elemental real(dp) function dabs(x) result(r)
-real(dp), intent(in) :: x
-if (x >= 0) then
-    r = x
-else
-    r = -x
-end if
-end function
+!elemental real(dp) function dabs(x) result(r)
+!real(dp), intent(in) :: x
+!if (x >= 0) then
+!    r = x
+!else
+!    r = -x
+!end if
+!end function
 
 ! sqrt -------------------------------------------------------------------------
 
-elemental real(sp) function ssqrt(x) result(r)
-real(sp), intent(in) :: x
-if (x >= 0) then
-    r = x**(1._sp/2)
-else
-    error stop "sqrt(x) for x < 0 is not allowed"
-end if
-end function
+!elemental real(sp) function ssqrt(x) result(r)
+!real(sp), intent(in) :: x
+!if (x >= 0) then
+!    r = x**(1._sp/2)
+!else
+!    error stop "sqrt(x) for x < 0 is not allowed"
+!end if
+!end function
 
-elemental real(dp) function dsqrt(x) result(r)
-real(dp), intent(in) :: x
-if (x >= 0) then
-    r = x**(1._dp/2)
-else
-    error stop "sqrt(x) for x < 0 is not allowed"
-end if
-end function
+!elemental real(dp) function dsqrt(x) result(r)
+!real(dp), intent(in) :: x
+!if (x >= 0) then
+!    r = x**(1._dp/2)
+!else
+!    error stop "sqrt(x) for x < 0 is not allowed"
+!end if
+!end function
 
 
 end module
