53804ecdb605523b11d6152074d5aea6bc386749
Rohit Goswami <rog32@hi.is>
2021-08-11 21:02:25 +0000

    intrin: Skeletal implementation (unused) [skip ci]

--- src/runtime/lfortran_intrinsic_array.f90
+++ src/runtime/lfortran_intrinsic_array.f90
@@ -1,4 +1,5 @@
 module lfortran_intrinsic_array
+use, intrinsic :: iso_fortran_env, only: sp => real32, dp => real64
 implicit none
 
 interface
@@ -53,5 +54,32 @@ interface
     integer, intent(in) :: kind
     end function
 end interface
+
+interface int
+   ! TODO: Handle arrays and more precision
+   module procedure iint, rint, iintk, rintk
+end interface
+
+contains
+
+! int --------------------------------------------------------------------------
+
+elemental integer(sp) function iint(x)
+integer, intent(in) :: x
+end function
+
+elemental integer(sp) function rint(x)
+real, intent(in) :: x
+end function
+
+elemental integer(sp) function iintk(x, kind)
+integer, intent(in) :: x(:)
+integer, intent(in) :: kind
+end function
+
+elemental integer(sp) function rintk(x, kind)
+real, intent(in) :: x(:)
+integer, intent(in) :: kind
+end function
 
 end module
