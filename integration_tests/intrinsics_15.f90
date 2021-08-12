9e68eeee8341471c7628fc95117aef9a5d58ce53
Rohit Goswami <rog32@hi.is>
2021-08-12 19:58:30 +0000

    tests: Rename and update [skip ci]

--- integration_tests/intrinsics_15.f90
+++
@@ -1,4 +0,0 @@
-program intrinsics_15
-  real, parameter :: x = 3.3
-  print*, int(x)
-end program
--- integration_tests/intrinsics_16.f90
+++
@@ -1,5 +0,0 @@
-program intrinsics_15
-  real, parameter :: x = 3.3
-  integer, parameter :: y = int(x)
-  print*, y
-end program
