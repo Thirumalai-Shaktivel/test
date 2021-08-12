48355f9f439d9a9ee3b36a63d7f5017e1481100b
Ondřej Čertík <ondrej@certik.us>
2021-08-12 12:59:53 -0600

    Add a sin implementation to math2

--- src/runtime/lfortran_intrinsic_math2.f90
+++ src/runtime/lfortran_intrinsic_math2.f90
@@ -2,6 +2,8 @@
 module lfortran_intrinsic_math2
 use, intrinsic :: iso_fortran_env, only: sp => real32, dp => real64
 implicit none
+private
+public abs, sqrt, sin
 
 interface abs
     module procedure sabs, dabs
@@ -11,6 +13,11 @@ interface sqrt
     module procedure ssqrt, dsqrt
 end interface
 
+interface sin
+    module procedure dsin
+end interface
+
+real(dp), parameter :: pi = 3.1415926535897932384626433832795_dp
 
 contains
 
@@ -54,5 +61,31 @@ else
 end if
 end function
 
+elemental real(dp) function dsin(x) result(r)
+real(dp), intent(in) :: x
+real(dp) :: y
+integer :: n
+y = modulo(x, 2*pi)
+y = min(y, pi - y)
+y = max(y, -pi - y)
+y = min(y, pi - y)
+r = kernel_dsin(y)
+end function
+
+! Accurate on [-pi/2,pi/2] to about 1e-16
+elemental real(dp) function kernel_dsin(x) result(res)
+real(dp), intent(in) :: x
+real(dp), parameter :: S1 = 0.9999999999999990771_dp
+real(dp), parameter :: S2 = -0.16666666666664811048_dp
+real(dp), parameter :: S3 = 8.333333333226519387e-3_dp
+real(dp), parameter :: S4 = -1.9841269813888534497e-4_dp
+real(dp), parameter :: S5 = 2.7557315514280769795e-6_dp
+real(dp), parameter :: S6 = -2.5051823583393710429e-8_dp
+real(dp), parameter :: S7 = 1.6046585911173017112e-10_dp
+real(dp), parameter :: S8 = -7.3572396558796051923e-13_dp
+real(dp) :: z
+z = x*x
+res = x * (S1+z*(S2+z*(S3+z*(S4+z*(S5+z*(S6+z*(S7+z*S8)))))))
+end function
 
 end module
