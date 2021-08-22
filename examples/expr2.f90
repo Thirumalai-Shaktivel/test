410a4de282350be71b3aee539f9d9d770f728287
Ondřej Čertík <ondrej@certik.us>
2021-08-22 15:48:18 -0600

    Try complex numbers

--- examples/expr2.f90
+++ examples/expr2.f90
@@ -2,9 +2,17 @@ program expr2
 implicit none
 
 integer :: x
+complex(4) :: c
+complex(8) :: z
 
 x = (2+3)*5
 print *, x
 print *, tan(0.5)
+c = (1.5, 3.5)
+print *, c
+print *, tan(c)
+z = (1.5_8, 3.5_8)
+print *, z
+print *, tan(z)
 
 end program
