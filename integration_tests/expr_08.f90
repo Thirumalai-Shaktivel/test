4c7fe54019aadf921eaecd3ab6c5669e6fa517b4
Ondřej Čertík <ondrej@certik.us>
2021-08-25 11:27:20 -0600

    Add a test for an array on the LHS

--- integration_tests/expr_08.f90
+++ integration_tests/expr_08.f90
@@ -1,6 +1,6 @@
 program expr_08
 implicit none
-integer :: x
+integer :: x, A(10), i
 real :: r
 x = 5
 print *, x
@@ -13,4 +13,14 @@ print *, r
 r += 3.5
 print *, r
 if (abs(r - 9) > 1e-5) error stop
+
+A = 0
+do i = 1, 10
+    A(i) += 3
+end do
+print *, A
+do i = 1, 10
+    A(i) += i
+end do
+print *, A
 end program
