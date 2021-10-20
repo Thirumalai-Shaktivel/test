bef85055b4a7325ee089894d10f32746a4becc73
Gagandeep Singh <gdp.1807@gmail.com>
2021-10-20 20:56:43 +0530

    Added a test for abstract interface

---
+++ integration_tests/interface_08.f90
@@ -0,0 +1,22 @@
+module m
+    abstract interface
+        subroutine sub(x,y)
+            integer, intent(in) :: x
+            integer, intent(in) :: y
+        end subroutine
+    end interface
+end module
+
+program main
+    use m
+    procedure (sub) sub1
+    procedure (sub), pointer :: p
+    p => sub1
+    call p(5, 10)
+end program
+
+subroutine sub1 (x, y)
+        integer ,intent(in) :: x
+        integer ,intent(in) :: y
+        print *, "the sum of x and y is: ", x + y
+end subroutine
