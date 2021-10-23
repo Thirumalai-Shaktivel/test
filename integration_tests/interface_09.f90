ce4392abe92f7ab0f81a6da12eb16dfcc773686c
Gagandeep Singh <gdp.1807@gmail.com>
2021-10-23 17:13:33 +0530

    improvised test

--- integration_tests/interface_09.f90
+++ integration_tests/interface_09.f90
@@ -1,21 +1,22 @@
-module m
+module interface_09_m
     abstract interface
-        subroutine sub(x,y)
+        subroutine add(x, y, z)
             integer, intent(in) :: x
             integer, intent(in) :: y
+            integer, intent(in) :: z
         end subroutine
     end interface
 end module
 
-program interface_08
-    use m, only : sub
-    procedure (sub) :: sub1
-    procedure (sub), pointer :: p
-    p => sub1
+program interface_09
+    use interface_09_m, only : add
+    procedure (add) :: add_def
+    procedure (add), pointer :: p
+    p => add_def
     call p(5, 10)
 end program
 
-subroutine sub1 (x, y)
+subroutine add_def(x, y)
         integer ,intent(in) :: x
         integer ,intent(in) :: y
         print *, "the sum of x and y is: ", x + y
