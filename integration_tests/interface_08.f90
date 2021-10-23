55721a15d420920b6e9cb0ebf32dc44bc3a523c9
Gagandeep Singh <gdp.1807@gmail.com>
2021-10-23 17:12:12 +0530

    split tests

--- integration_tests/interface_08.f90
+++ integration_tests/interface_08.f90
@@ -1,4 +1,4 @@
-module m
+module interface_08
     abstract interface
         subroutine sub(x,y)
             integer, intent(in) :: x
@@ -6,17 +6,3 @@ module m
         end subroutine
     end interface
 end module
-
-program interface_08
-    use m, only : sub
-    procedure (sub) :: sub1
-    procedure (sub), pointer :: p
-    p => sub1
-    call p(5, 10)
-end program
-
-subroutine sub1 (x, y)
-        integer ,intent(in) :: x
-        integer ,intent(in) :: y
-        print *, "the sum of x and y is: ", x + y
-end subroutine
