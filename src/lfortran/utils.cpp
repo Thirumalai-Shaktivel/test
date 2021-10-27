2d87d243fd6191e242a67a26b1f93c0d7db3fd30
Ondřej Čertík <ondrej@certik.us>
2021-10-26 18:03:44 -0600

    Disable whereami

--- src/lfortran/utils.cpp
+++ src/lfortran/utils.cpp
@@ -16,10 +16,11 @@ void get_executable_path(std::string &executable_path, int &dirname_length)
 {
     int length;
 
-    length = wai_getExecutablePath(NULL, 0, &dirname_length);
+    //length = wai_getExecutablePath(NULL, 0, &dirname_length);
+    length = 0;
     if (length > 0) {
         std::string path(length+1, '\0');
-        wai_getExecutablePath(&path[0], length, &dirname_length);
+        //wai_getExecutablePath(&path[0], length, &dirname_length);
         executable_path = path;
         if (executable_path[executable_path.size()-1] == '\0') {
             executable_path = executable_path.substr(0,executable_path.size()-1);
