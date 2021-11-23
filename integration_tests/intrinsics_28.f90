a956555bd0ed4ddca4071ee4486ce8da4f6a61a2
Gagandeep Singh <gdp.1807@gmail.com>
2021-11-23 16:13:25 +0530

    Added test case for new_line intrinsic

--- integration_tests/intrinsics_28.f90
+++ integration_tests/intrinsics_28.f90
@@ -1,9 +1,4 @@
-program debug
+program intrinsics_29
     implicit none
-    integer, parameter :: a = 10, b = -20, c = 30
-    integer, parameter :: d = -40, e = 0, f = 50
-    print *, min(a, b, c, d, e, f)
-    print *, max(a, b, c, d, e, f)
-    print *, max(a, b)
-    print *, min(a, b)
-end program
+    print *, 'This is record 1.'//new_line('A')//'This is record 2.'
+  end program intrinsics_29
