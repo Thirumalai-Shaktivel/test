0d03b0df8ac30b35cba57afcfe552dcfab837273
Gagandeep Singh <gdp.1807@gmail.com>
2021-11-27 16:25:22 +0530

    Updated ieee_arithemtic

--- src/runtime/pure/lfortran_intrinsic_ieee_arithmetic.f90
+++ src/runtime/pure/lfortran_intrinsic_ieee_arithmetic.f90
@@ -2,19 +2,19 @@ module lfortran_intrinsic_ieee_arithmetic
     use, intrinsic :: iso_fortran_env, only: sp => real32, dp => real64
     implicit none
 
-    type IEEE_CLASS_TYPE
+    type ieee_class_type
     end type
 
-    type(ieee_class_type), parameter :: ieee_negative_denormal
-    type(ieee_class_type), parameter :: ieee_negative_inf
-    type(ieee_class_type), parameter :: ieee_negative_normal
-    type(ieee_class_type), parameter :: ieee_negative_zero
-    type(ieee_class_type), parameter :: ieee_positive_denormal
-    type(ieee_class_type), parameter :: ieee_positive_inf
-    type(ieee_class_type), parameter :: ieee_positive_normal
-    type(ieee_class_type), parameter :: ieee_positive_zero
-    type(ieee_class_type), parameter :: ieee_quiet_nan
-    type(ieee_class_type), parameter :: ieee_signaling_nan
+    type(ieee_class_type) :: ieee_negative_denormal
+    type(ieee_class_type) :: ieee_negative_inf
+    type(ieee_class_type) :: ieee_negative_normal
+    type(ieee_class_type) :: ieee_negative_zero
+    type(ieee_class_type) :: ieee_positive_denormal
+    type(ieee_class_type) :: ieee_positive_inf
+    type(ieee_class_type) :: ieee_positive_normal
+    type(ieee_class_type) :: ieee_positive_zero
+    type(ieee_class_type) :: ieee_quiet_nan
+    type(ieee_class_type) :: ieee_signaling_nan
 
     interface ieee_class
         module procedure spieee_class, dpieee_class
@@ -31,34 +31,34 @@ module lfortran_intrinsic_ieee_arithmetic
     contains
 
     elemental function spieee_class(x) result(y)
-        real(sp) :: x
+        real(sp), intent(in) :: x
         type(ieee_class_type) :: y
     end function
 
     elemental function dpieee_class(x) result(y)
-        real(dp) :: x
+        real(dp), intent(in) :: x
         type(ieee_class_type) :: y
     end function
 
-    elemental function spieee_value(x, class) result(y)
-        real(sp) :: x
-        type(ieee_class_type) :: class
+    elemental function spieee_value(x, cls) result(y)
+        real(sp), intent(in) :: x
+        type(ieee_class_type), intent(in) :: cls
         real(sp) :: y
     end function
 
-    elemental function dpieee_value(x, class) result(y)
-        real(dp) :: x
-        type(ieee_class_type) :: class
+    elemental function dpieee_value(x, cls) result(y)
+        real(dp), intent(in) :: x
+        type(ieee_class_type), intent(in) :: cls
         real(dp) :: y
     end function
 
     elemental function spieee_is_nan(x) result(y)
-        real(sp) :: x
+        real(sp), intent(in) :: x
         logical :: y
     end function
 
     elemental function dpieee_is_nan(x) result(y)
-        real(dp) :: x
+        real(dp), intent(in) :: x
         logical :: y
     end function
 
