fe9f18cb4c80d940d687ccb3ff12c2dda88d94b8
Gagandeep Singh <gdp.1807@gmail.com>
2021-12-22 19:56:07 +0530

    Updated functions_04 and reference tests

--- integration_tests/functions_04.f90
+++ integration_tests/functions_04.f90
@@ -1,22 +1,32 @@
-module stdlib_ascii
+module stdlib_int
 
     implicit none
 
-    interface to_lower
-        module procedure :: to_lower
-    end interface to_lower
+    interface to_negative
+        module procedure :: to_negative
+    end interface to_negative
 
 contains
 
-    pure function to_lower(string) result(lower_string)
-        character(len=*), intent(in) :: string
-        character(len=len(string)) :: lower_string
-        integer :: i
+    pure function to_negative(int) result(neg_int)
+        integer, intent(in) :: int
+        integer :: neg_int
 
-        do i = 1, len(string)
-            lower_string(i:i) = string(i:i)
-        end do
+        if (int >= 0) then
+            neg_int = -int
+        end if
 
-    end function to_lower
+    end function to_negative
 
-end module stdlib_ascii
+end module stdlib_int
+
+program functions_04
+    use stdlib_int
+    implicit none
+
+    integer :: int = 4
+    if (to_negative(int) /= -4) error stop
+    int = -4
+    if (to_negative(int) /= -4) error stop
+
+end program
