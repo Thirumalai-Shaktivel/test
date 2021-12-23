f7d7db569d3e8b161680e1f5e1efa1d953626f15
Ondřej Čertík <ondrej@certik.us>
2021-12-23 11:01:03 +0000

    Corrected test according to the use case

--- integration_tests/flip_sign.f90
+++ integration_tests/flip_sign.f90
@@ -1,14 +1,17 @@
 program flip_sign
     implicit none
     integer :: number
+    real :: x
 
     number = 123
+    x = 5.5
 
-    if (modulo(number, 2) == 1 ) number = -number
-    if (number /= -123) error stop
+    if (modulo(number, 2) == 1 ) x = -x
+    if (abs(x - (-5.5)) > eps) error stop
 
     number = 124
-    if (modulo(number, 2) == 1 ) number = -number
-    if (number /= 124) error stop
+    x = 5.5
+    if (modulo(number, 2) == 1 ) x = -x
+    if (abs(x - (5.5)) > eps) error stop
 
 end program
