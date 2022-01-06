23bdc5886cc76cac3900315e2d2d5636f0871191
Gagandeep Singh <gdp.1807@gmail.com>
2022-01-06 16:35:26 +0530

    updated reference tests

--- integration_tests/flip_sign.f90
+++ integration_tests/flip_sign.f90
@@ -1,17 +1,17 @@
 program flip_sign
     implicit none
     integer :: number
-    real :: x
+    real :: x, eps = 1e-6
 
     number = 123
     x = 5.5
 
     if (modulo(number, 2) == 1 ) x = -x
-    ! if (abs(x - (-5.5)) > eps) error stop
+    if (abs(x - (-5.5)) > eps) error stop
 
-    ! number = 124
-    ! x = 5.5
-    ! if (modulo(number, 2) == 1 ) x = -x
-    ! if (abs(x - (5.5)) > eps) error stop
+    number = 124
+    x = 5.5
+    if (modulo(number, 2) == 1 ) x = -x
+    if (abs(x - (5.5)) > eps) error stop
 
 end program
