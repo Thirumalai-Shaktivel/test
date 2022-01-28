1439ddbae5bcf9a57f83a7d26869b367c23647b9
Gagandeep Singh <gdp.1807@gmail.com>
2022-01-28 15:38:20 +0530

    Fixed div_to_mul according latest refactoring

--- src/libasr/pass/div_to_mul.cpp
+++ src/libasr/pass/div_to_mul.cpp
@@ -36,61 +36,15 @@ to:
 class DivToMulVisitor : public PassUtils::PassVisitor<DivToMulVisitor>
 {
 private:
-    Allocator &al;
     ASR::TranslationUnit_t &unit;
 
     std::string rl_path;
-    Vec<ASR::stmt_t*> div_to_mul_result;
-
-    SymbolTable* current_scope;
 
 public:
-    DivToMulVisitor(Allocator &al_, ASR::TranslationUnit_t &unit_, const std::string& rl_path_) : al(al_), unit(unit_),
-    rl_path{rl_path_}
+    DivToMulVisitor(Allocator &al_, ASR::TranslationUnit_t &unit_, const std::string& rl_path_) : PassVisitor(al_, nullptr), unit(unit_),
+    rl_path(rl_path_)
     {
-        div_to_mul_result.reserve(al, 1);
-    }
-
-    // TODO: Only Program and While is processed, we need to process all calls
-    // to visit_stmt().
-
-    void visit_Program(const ASR::Program_t &x) {
-        current_scope = x.m_symtab;
-
-        // Transform nested functions and subroutines
-        for (auto &item : x.m_symtab->scope) {
-            if (is_a<ASR::Subroutine_t>(*item.second)) {
-                ASR::Subroutine_t *s = ASR::down_cast<ASR::Subroutine_t>(item.second);
-                visit_Subroutine(*s);
-            }
-            if (is_a<ASR::Function_t>(*item.second)) {
-                ASR::Function_t *s = down_cast<ASR::Function_t>(item.second);
-                visit_Function(*s);
-            }
-        }
-
-        // FIXME: this is a hack, we need to pass in a non-const `x`,
-        // which requires to generate a TransformVisitor.
-        ASR::Program_t &xx = const_cast<ASR::Program_t&>(x);
-
-        transform_stmts(xx.m_body, xx.n_body, al, div_to_mul_result);
-
-    }
-
-    void visit_Subroutine(const ASR::Subroutine_t &x) {
-        // FIXME: this is a hack, we need to pass in a non-const `x`,
-        // which requires to generate a TransformVisitor.
-        ASR::Subroutine_t &xx = const_cast<ASR::Subroutine_t&>(x);
-        current_scope = xx.m_symtab;
-        transform_stmts(xx.m_body, xx.n_body, al, div_to_mul_result);
-    }
-
-    void visit_Function(const ASR::Function_t &x) {
-        // FIXME: this is a hack, we need to pass in a non-const `x`,
-        // which requires to generate a TransformVisitor.
-        ASR::Function_t &xx = const_cast<ASR::Function_t&>(x);
-        current_scope = xx.m_symtab;
-        transform_stmts(xx.m_body, xx.n_body, al, div_to_mul_result);
+        pass_result.reserve(al, 1);
     }
 
     void visit_BinOp(const ASR::BinOp_t& x) {
