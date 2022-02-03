2a0a2dc9ef05a19d2e966c622133e1ad7241d336
Gagandeep Singh <gdp.1807@gmail.com>
2022-02-03 19:58:17 +0530

    Added a test for FMA optimization pass

---
+++ integration_tests/fma.f90
@@ -0,0 +1,13 @@
+program fma
+
+    implicit none
+    real :: x = 4.0
+    real, parameter :: pi = 3.14
+    real(8) :: S1 = 1.0, S2 = 2.0
+    real(8) :: z = 1.0
+    x = (x * 4 + 0.5_4*sign(1._4, x)) - x * pi
+    z = S1+z*S2
+    print *, x, z
+
+end program
+
