e7b6610f49ce6171552ceed8766da73be675d73f
Ubaid <shaikhubaid769@gmail.com>
2022-04-18 11:20:17 +0530

    Uncomment em++ command in build2.sh

--- build2.sh
+++ build2.sh
@@ -8,6 +8,6 @@ make -j16 wasm_demo
 # Use -Oz for small size
 
 # cd src/bin
+em++ -Wall -Wextra -fexceptions @CMakeFiles/wasm_demo.dir/objects1.rsp -o wasm_demo.html @CMakeFiles/wasm_demo.dir/linklibs.rsp -s EXPORTED_RUNTIME_METHODS='["ccall", "cwrap"]' --shell-file shell_minimal.html
 # em++ -Wall -Wextra -fexceptions wasm_demo -o wasm_demo.html -s EXPORTED_RUNTIME_METHODS='["ccall", "cwrap"]' --shell-file shell_minimal.html
-# em++ -Wall -Wextra -fexceptions @CMakeFiles/wasm_demo.dir/objects1.rsp -o wasm_demo.html @CMakeFiles/wasm_demo.dir/linklibs.rsp -s EXPORTED_RUNTIME_METHODS='["ccall", "cwrap"]' --shell-file shell_minimal.html
 # emcc -Wall -Wextra -fexceptions @CMakeFiles/wasm_demo.dir/objects1.rsp -o wasm_demo.html -s EXPORTED_RUNTIME_METHODS='["ccall", "cwrap"]' --shell-file shell_minimal.html
